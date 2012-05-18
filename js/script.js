079f0096c9e5a14bba1663940709da67b4f10941
Clippit Zhang <zlthooray@gmail.com>
2012-05-18 22:05:51 +0800

    some animation(not done yet)

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 */
 
 (function(){
+
 	$('.nav-link').hover(
 		function() {
 			var $this = $(this);
@@ -13,6 +14,24 @@
 			$this.text($this.data("en"));
 		}
 	);
+
+	var $main = $("#main"),
+		$logo = $("#logo"),
+		$container = $(".content-container");
+		logo_top_offset = $logo.position().top + $logo.height();
+
+	$('.nav-link').on('click', function(event) {
+		event.preventDefault();
+		var $this = $(this),
+			target = $this.data("en").toLowerCase();
+		$main.animate(
+			{"top": - logo_top_offset, "height": "+=" + logo_top_offset / 2},
+			1000
+		);
+		$("#" + target).show();
+		$container.animate({'height': 'show'}, 1000);
+	});
+
 }());
 
 
