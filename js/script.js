f61bb4b3a4bfdf3983b31621f098721ac241f0f9
Clippit Zhang <zlthooray@gmail.com>
2012-05-19 22:03:55 +0800

    better expansion, add overlay

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 
 (function(){
 
-	$('.nav-link').hover(
+/*	$('.nav-link').hover(
 		function() {
 			var $this = $(this);
 			$this.text($this.data("cn"));
@@ -14,28 +14,47 @@
 			$this.text($this.data("en"));
 		}
 	);
+*/
 
 	var $main = $("#main"),
 		$logo = $("#logo"),
 		$container = $(".content-container"),
-		logo_top_offset = $logo.position().top + $logo.height();
+		logo_top_offset = $logo.position().top + $logo.height(),
 		window_height = $(window).height();
 
-	$main.css('height', window_height);
+	$main.css('height', window_height);  // fix css3 animation
 	$container.css('max-height', Math.min(window_height * 0.8, window_height - $("nav").height()));
 
 	$('.nav-link').on('click', function(event) {
 		event.preventDefault();
+		if ($(".content:hidden").length != $(".content").length) {
+			return;
+		}
 		var $this = $(this),
 			target = $this.data("en").toLowerCase(),
-			duration = 1000;
+			duration = 1200,
+			overlay = $('<div class="overlay"> </div>');
+
+		$('nav').after(overlay);
+		overlay.on('click', closeFolder);
+
+		$this.text($this.data("cn")).addClass("active");
+		$(".nav-link:not(.active)").addClass("disabled");
+
 		$main.animate(
 			{"top": - logo_top_offset * 0.85, "height": "+=" + logo_top_offset},
 			duration
 		);
 		$("#" + target).show();
-		$container.animate({'height': 'show'}, 1000);
+		$container.animate({'height': 'show'}, duration);
+		overlay.animate({opacity: 0.5}, duration);
 	});
+
+	function closeFolder() {
+		console.log(this);
+	}
+
+
 
 }());
 
